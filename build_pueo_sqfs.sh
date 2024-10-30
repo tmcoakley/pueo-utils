1316007a49018c1fd1ed063a9c5c4b3eae7b5bce
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2024-10-30 16:40:15 -0400

    is it working?? who knows

---
+++ build_pueo_sqfs.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# individual single-file python modules
+PYTHON_SINGLE_FILES = "pysoceeprom/pysoceeprom.py \
+	        pyzynqmp/pyzynqmp.py"
+# multi-file python modules wrapped in directories
+PYTHON_DIRS = "pyrfdc/ \
+	       s6clk/ "
+
+SCRIPTS = "scripts/build_squashfs"
+
+if [ "$#" -ne 1 ] ; then
+    echo "usage: build_pueo_sqfs.sh <destination filename>"
+    exit 1
+fi
+
+DEST=$1
+WORKDIR=$(mktemp -d)
+
+cp -R base_squashfs/* ${WORKDIR}
+for f in ${PYTHON_SINGLE_FILES} ; do
+    cp $f ${WORKDIR}/pylib/
+done
+for d in ${PYTHON_DIRS} ; do
+    cp -R $d ${WORKDIR}/pylib/
+done
+for s in ${SCRIPTS} ; do
+    cp $s ${WORKDIR}/bin/
+done
+
+mksquashfs ${WORKDIR} $1
+rm -rf ${WORKDIR}
+

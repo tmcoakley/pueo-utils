173eb64c12db4fc4a09cfb4657b2e1b214e1734d
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2024-12-13 12:26:40 -0500

    switch to pipe-y select-y things and lots of bugfixes

--- pysurfHskd/surfStartupHandler.py
+++ pysurfHskd/surfStartupHandler.py
@@ -1,31 +1,36 @@
 from enum import Enum
 import logging
+import os
+from pueo.common.bf import bf
 
 # the startup handler actually runs in the main
-# thread: it takes two queues, the primary event
-# queue and the tick event queue.
-# Note that basically whenever we wait, we
-# run next tick because either it's going to be
-# a long wait, or it never should've had us wait
-# at all and so it's actually an error.
+# thread. it either writes a byte to a pipe to
+# indicate that it should be called again,
+# or it pushes its run function into the tick FIFO
+# if it wants to be called when the tick FIFO
+# expires.
+# the tick FIFO takes closures now
+# god this thing is a headache
 class StartupHandler:
     LMK_FILE = "/usr/local/share/SURF6_LMK.txt"
         
     class StartupState(int, Enum):
         STARTUP_BEGIN = 0
         WAIT_CLOCK = 1
-        PROGRAM_ACLK = 2
-        WAIT_ACLK_LOCK = 3
-        ENABLE_ACLK = 4
-        WAIT_PLL_LOCK = 5
-        ALIGN_RXCLK = 6
-        LOCATE_EYE = 7
-        TURFIO_LOCK = 8
-        WAIT_TURFIO_LOCKED = 9
-        ENABLE_TRAIN = 10
-        WAIT_TURFIO = 11
-#        DISABLE_TRAIN = 12
-        STARTUP_FAILURE = -1
+        RESET_CLOCK = 2
+        RESET_CLOCK_DELAY = 3
+        PROGRAM_ACLK = 4
+        WAIT_ACLK_LOCK = 5
+        ENABLE_ACLK = 6
+        WAIT_PLL_LOCK = 7
+        ALIGN_RXCLK = 8
+        LOCATE_EYE = 9
+        TURFIO_LOCK = 10
+        WAIT_TURFIO_LOCKED = 11
+        ENABLE_TRAIN = 12
+        WAIT_TURFIO = 13
+        DISABLE_TRAIN = 14
+        STARTUP_FAILURE = 255
 
         def __index__(self) -> int:
             return self.value
@@ -36,34 +41,33 @@ class StartupHandler:
                  surfClock,
                  surfClockReset,
                  autoHaltState,
-                 eventFifo,
-                 tickFifo,
-                 eventType):
+                 tickFifo):
         self.state = self.StartupState.STARTUP_BEGIN
         self.logger = logging.getLogger(logName)
         self.surf = surfDev
         self.clock = surfClock
-        surf.clockReset = surfClockReset
-        self.endState = autoHaltState
-        self.immediate = eventFifo
+        self.clockReset = surfClockReset
+        self.endState = autoHaltState        
         self.tick = tickFifo
-        self.eventType = eventType
+        self.rfd, self.wfd = os.pipe2(os.O_NONBLOCK | os.O_CLOEXEC)
         if self.endState is None:
             self.endState = self.StartupState.STARTUP_BEGIN
 
     def _runNextTick(self):
         if not self.tick.full():
-            self.tick.put((self.eventType, NULL))
+            self.tick.put(self.run)
         else:
             raise RuntimeError("tick FIFO became full in handler!!")
 
     def _runImmediate(self):
-        if not self.immediate.full():
-            self.immediate.put((self.eventType, NULL))
-        else:
-            raise RuntimeError("immediate FIFO became full in handler!!")
+        toWrite = (self.state).to_bytes(1, 'big')
+        nb = os.write(self.wfd, toWrite)
+        if nb != len(toWrite):
+            raise RuntimeError("could not write to pipe!")
         
     def run(self):
+        # whatever dumb debugging
+        self.logger.trace("startup state: %s", self.state)
         # endState is used to allow us to single-step
         # so if you set startup to 0 in the EEPROM, you can
         # set the end state via HSK and single-step through
@@ -80,7 +84,7 @@ class StartupHandler:
                 return
             else:
                 dv = self.surf.DateVersion(self.surf.read(0x4))
-                self.logger.info("this is SURF", str(dv))
+                self.logger.info("this is SURF %s", str(dv))
                 self.state = self.StartupState.WAIT_CLOCK
                 self._runImmediate()
                 return
@@ -91,23 +95,35 @@ class StartupHandler:
                 return
             else:
                 self.logger.info("RACKCLK is ready.")                
-                self.state = self.StartupState.PROGRAM_ACLK
+                self.state = self.StartupState.RESET_CLOCK
                 self._runImmediate()
                 return
-        elif self.state == self.StartupState.PROGRAM_ACLK:
-            if not os.path.exists(LMK_FILE):
-                self.logger.error("failed locating %s" % LMK_FILE)
+        elif self.state == self.StartupState.RESET_CLOCK:
+            if not os.path.exists(self.LMK_FILE):
+                self.logger.error("failed locating %s", self.LMK_FILE)
                 self.state = self.StartupState.STARTUP_FAILURE
                 self._runNextTick()
-                return
             self.clockReset.write(1)
             self.clockReset.write(0)
-            self.clock.surfClock.configure(LMK_FILE)
+            self.state = self.StartupState.RESET_CLOCK_DELAY
+            self._runNextTick()
+            return
+        elif self.state == self.StartupState.RESET_CLOCK_DELAY:
+            self.clock.surfClockInit()            
+            self.state = self.StartupState.PROGRAM_ACLK
+            self._runNextTick()
+            return
+        elif self.state == self.StartupState.PROGRAM_ACLK:
+            # debugging
+            st = self.clock.surfClock.status()
+            self.logger.detail("Clock status before programming: %2.2x", st)
+            self.clock.surfClock.configure(self.LMK_FILE)
             self.state = self.StartupState.WAIT_ACLK_LOCK
             self._runImmediate()
             return
         elif self.state == self.StartupState.WAIT_ACLK_LOCK:
             st = self.clock.surfClock.status()
+            self.logger.detail("Clock status now: %2.2x", st)
             if st & 0x2 == 0:
                 self._runNextTick()
                 return
@@ -128,7 +144,7 @@ class StartupHandler:
             self.state = self.StartupState.WAIT_PLL_LOCK
             self._runImmediate()
             return
-        elif self.state == self.StartupState.WAIT_PLL_LOCK
+        elif self.state == self.StartupState.WAIT_PLL_LOCK:
             rv = bf(self.surf.read(0x800))
             if not rv[14]:
                 self._runNextTick()
@@ -139,7 +155,8 @@ class StartupHandler:
         elif self.state == self.StartupState.ALIGN_RXCLK:
             # use firmware parameters for this eventually!!!
             # this needs to freaking do something if it fails!!
-            self.surf.align_rxclk()
+            av = self.surf.align_rxclk()
+            self.logger.info("RXCLK aligned at offset %f", av)
             self.state = self.StartupState.LOCATE_EYE
             self._runImmediate()
             return

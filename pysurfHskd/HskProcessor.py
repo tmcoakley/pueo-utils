a60ea261838f0e9b4ea760eae715072f07423607
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2024-12-21 14:06:45 -0500

    pass softnext path to HskProcessor

--- pysurfHskd/HskProcessor.py
+++ pysurfHskd/HskProcessor.py
@@ -2,8 +2,16 @@ import logging
 import os
 from subprocess import Popen, PIPE, TimeoutExpired
 from pathlib import Path
+import pickle
 
 class HskProcessor:
+    kReboot = 0xFF
+    kTerminate = 0xFE
+    bmKeepCurrentSoft = 0x1
+    bmRevertChanges = 0x2
+    bmCleanup = 0x4
+    bmForceReprogram = 0x8
+    bmMagicValue = 0x80
     def ePingPong(self, pkt):
         rpkt = bytearray(pkt)
         rpkt[1] = rpkt[0]
@@ -40,18 +48,30 @@ class HskProcessor:
         rpkt[12] = (256-sum(rpkt[4:12])) & 0xFF
         self.hsk.sendPacket(rpkt)
 
-    # identify sends the PS DNA+MAC addr+slot ident if any (not fixed len)
+    # identify sends
+    # PL DNA
+    # MAC
+    # plxVersion
+    # sqfs version if any
+    # slot identifier if any
+    # tends to be around 75 bytes or so
     def eIdentify(self, pkt):
         rpkt = bytearray(4)
         rpkt[1] = pkt[0]
         rpkt[0] = self.hsk.myID
         rpkt[2] = 18
-        rpkt += self.zynq.dna.encode()
-        rpkt += self.zynq.mac.encode()
+        # fixed length
+        rpkt += self.zynq.dna.encode() + b'\x00'
+        rpkt += self.zynq.mac.encode() + b'\x00'
+        # this part is not
+        rpkt += self.plxVersion
+        # remainder is optional
+        v = self.version
+        if v is not None:
+            rpkt += b'\x00' + v
         l = self.eeprom.location
         if l is not None:
-            rpkt += l['crate']
-            rpkt += l['slot']
+            rpkt += b'\x00' + l['crate'] + l['slot']
         rpkt[3] = len(rpkt[4:])
         cks = (256 - sum(rpkt[4:])) & 0xFF
         rpkt.append(cks)
@@ -68,31 +88,93 @@ class HskProcessor:
         rpkt[4] = self.startup.state
         rpkt[5] = (256 - rpkt[4]) & 0xFF
         self.hsk.sendPacket(rpkt)
+        
+    @staticmethod
+    def _getSoftTimestamp(fn: bytes):
+        cmd = ["unsquashfs", "-fstime", fn.decode()]
+        p = Popen(cmd, stdin=PIPE, stdout=PIPE)
+        r = p.communicate()
+        if p.returncode == 0:
+            return r[0].strip(b'\n')
+        return b''
+        
+    def eSoftNext(self, pkt):
+        rpkt = bytearray(4)
+        rpkt[1] = pkt[0]
+        rpkt[0] = self.hsk.myID
+        linkname = b''
+        timestamp = b''
+        if len(pkt) > 5:
+            fn = pkt[4:-1]
+            fp = Path(fn.decode())
+            if fn[0] == 0:
+                if self.nextSoft.exists():
+                    self.nextSoft.unlink()
+            else:
+                # want to set it, so do sanity check
+                if fp.is_file():
+                    timestamp = self._getSoftTimestamp(fn)
+                if timestamp == b'':
+                    # failed sanity check
+                    rpkt[2] = 0xFF
+                    rpkt[3] = 0
+                    rpkt.append(0)
+                    self.hsk.sendPacket(rpkt)
+                    return
+                # replace the link
+                if self.nextSoft.exists():
+                    self.nextSoft.unlink()
+                self.nextSoft.symlink_to(fp)
+                linkname = fn
+        else:
+            # just reading it
+            if self.nextSoft.exists():
+                if not self.nextSoft.is_symlink():
+                    logger.error("%s is not a link! Deleting it!!",
+                                 self.nextSoft.name)
+                    self.nextSoft.unlink()
+                else:
+                    linkname = bytes(self.nextSoft.readlink())
+                    timestamp = self._getSoftTimestamp(linkname)
+        rpkt[2] = 135
+        rpkt += linkname + b'\x00' + timestamp
+        rpkt[3] = len(rpkt[4:])
+        cks = (256 - sum(rpkt[4:])) & 0xFF
+        rpkt.append(cks)
+        self.hsk.sendPacket(rpkt)                    
 
+    # so much more error checking
     def eFwNext(self, pkt):
         rpkt = bytearray(4)
         rpkt[1] = pkt[0]
         rpkt[0] = self.hsk.myID
         if len(pkt) > 5:
             fn = pkt[4:-1]
+            fp = Path(fn.decode())
             if fn[0] == 0:
-                os.unlink(self.zynq.NEXT)
-            elif not os.path.isfile(fn):
+                if self.nextFw.exists():
+                    self.nextFw.unlink()
+            elif not fp.is_file():
                 rpkt[2] = 255
                 rpkt[3] = 0
                 rpkt.append(0)
                 self.hsk.sendPacket(rpkt)
                 return
             else:
-                os.unlink(self.zynq.NEXT)
-                os.symlink(fn, self.zynq.NEXT)
-        rpkt[2] = 129
-        if not os.path.exists(self.zynq.NEXT):
+                if self.nextFw.exists():
+                    self.nextFw.unlink()
+                self.nextFw.symlink_to(fp)
+        rpkt[2] = 129        
+        if not self.nextFw.exists() or not self.nextFw.is_symlink():
+            if self.nextFw.exists():
+                self.logger.error("%s is not a link! Deleting it!!",
+                                  self.zynq.NEXT)
+                self.nextFw.unlink()
             rpkt[3] = 1
             rpkt += b'\x00\x00'
             self.hsk.sendPacket(rpkt)
         else:
-            fn = os.readlink(self.zynq.NEXT).encode()+b'\x00'
+            fn = bytes(self.nextFw.readlink())
             rpkt += fn
             cks = (256 - sum(rpkt[4:])) & 0xFF
             rpkt.append(cks)
@@ -107,15 +189,8 @@ class HskProcessor:
         d = pkt[4:-1]
         if len(d):
             st = d[0]
-            if st == 0:
-                os.system("systemctl stop pyfwupd")
-            else:
-                os.unlink("/tmp/pyfwupd.loglevel")
-                if st & 0x80:
-                    loglevel = st & 0x7F
-                    Path("/tmp/pyfwupd.loglevel").write_text(str(loglevel))
-                os.system("systemctl start pyfwupd")
-        rpkt[4] = 0 if os.system("systemctl is-active --quiet pyfwupd") else 1
+            self._downloadMode(st)
+        rpkt[4] = self._downloadState()
         rpkt[5] = (256 - rpkt[4]) & 0xFF
         self.hsk.sendPacket(rpkt)
         
@@ -143,7 +218,25 @@ class HskProcessor:
         cks = (256 - sum(rpkt[4:])) & 0xFF
         rpkt.append(cks)
         self.hsk.sendPacket(rpkt)
-            
+
+    # no reply, and only check length/magic no
+    def eRestart(self, pkt):
+        d = pkt[4:-1]
+        # fake an error if you didn't tell me what to do
+        code = 0x80 if not len(d) else d[0]
+        if code & self.bmMagicValue:
+            if code != self.kReboot and code != self.kTerminate:
+                rpkt = bytearray(5)
+                rpkt[1] = pkt[0]
+                rpkt[0] = hsk.myID
+                rpkt[2] = 0xFF
+                rpkt[3] = 0
+                rpkt[4] = 0
+                self.hsk.sendPacket(rpkt)
+                return
+        self.restartCode = code
+        self.terminate()        
+        
     hskMap = { 0 : ePingPong,
                15 : eStatistics,
                16 : eVolts,
@@ -151,8 +244,10 @@ class HskProcessor:
                18 : eIdentify,
                32 : eStartState,
                129 : eFwNext,
+               135 : eSoftNext,
                189 : eJournal,
-               190 : eDownloadMode
+               190 : eDownloadMode,
+               191 : eRestart
               }
 
     # this guy is like practically the whole damn program
@@ -162,14 +257,53 @@ class HskProcessor:
                  eeprom,
                  startup,
                  logName,
-                 terminateFn):
+                 terminateFn,
+                 softNextFile="/tmp/pueo/next",
+                 plxVersionFile=None
+                 versionFile=None):
         self.hsk = hsk
         self.zynq = zynq
         self.eeprom = eeprom
         self.startup = startup
         self.logger = logging.getLogger(logName)
         self.terminate = terminateFn
+        self.restartCode = None
+        self.nextSoft = Path(softNextFile)
+        self.nextFw = Path(self.zynq.NEXT)
+        self.plxVersion = b''
+        if plxVersionFile:
+            p = Path(plxVersionFile)
+            if p.is_file():
+                self.plxVersion = p.read_text().strip("\n").encode()
+
+        v = None
+        if versionFile:
+            try:
+                with open(versionFile, 'rb') as f:
+                    pv = pickle.load(f)
+                v = pv['version'].encode() + b'\x00'
+                v += pv['hash'].encode() + b'\x00'
+                v += pv['date'].encode()
+            except Exception as e:
+                self.logger.error("Exception loading version: %s", repr(e))
+        self.version = v            
         self.journal = b''
+
+    def _downloadMode(self, st):
+        if st == 0:
+            os.system("systemctl stop pyfwupd")
+        else:
+            os.unlink("/tmp/pyfwupd.loglevel")
+            if st & 0x80:
+                loglevel = st & 0x7F
+                Path("/tmp/pyfwupd.loglevel").write_text(str(loglevel))
+            os.system("systemctl start pyfwupd")
+        
+    def _downloadState(self):
+        return 0 if os.system("systemctl is-active --quiet pyfwupd") else 1
+
+    def stop(self):
+        self._downloadMode(0)
         
     def basicHandler(self, fd, mask):
         if self.hsk.fifo.empty():

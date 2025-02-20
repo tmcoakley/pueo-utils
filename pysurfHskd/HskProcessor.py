a62d51a15858a10de47bc5b495819c9ab8a8ba7b
Taylor Coakley <coakley.64@osu.edu>
2025-02-19 19:00:38 -0500

    fixed issue with eStatistics

--- pysurfHskd/HskProcessor.py
+++ pysurfHskd/HskProcessor.py
@@ -25,7 +25,7 @@ class HskProcessor:
         rpkt[0] = self.hsk.myID
         rpkt[2] = 15
         rpkt[3] = 4
-        rpkt[4:8] = hsk.statistics()
+        rpkt[4:8] = self.hsk.statistics()
         rpkt[8] = (256-sum(rpkt[4:8])) & 0xFF
         self.hsk.sendPacket(rpkt)
     

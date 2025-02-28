016b160220808d011e76926b9d2f9fe82b8df3f4
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2025-02-28 05:16:26 +0000

    add tclsh8.5 to package.sh

--- package.sh
+++ package.sh
@@ -3,6 +3,7 @@
 # create a tarball of the necessary files for standalone xsdb
 
 tar --transform="s,bin/unwrapped/lnx64.o,xsdb," -cf standalone_xsdb.tar bin/unwrapped/lnx64.o/rdi_xsdb
+tar --transform="s,bin/unwrapped/lnx64.o,xsdb," -rf standalone_xsdb.tar bin/unwrapped/lnx64.o/tclsh8.5
 tar --transform="s,lib/lnx64.o,xsdb," -rf standalone_xsdb.tar lib/lnx64.o/libtcl8.5.so
 tar --transform="s,lib/lnx64.o,xsdb," -rf standalone_xsdb.tar lib/lnx64.o/libtcltcf.so
 tar --transform="s,tps/tcl,xsdb/tclLib," -rf standalone_xsdb.tar tps/tcl/tcl8.5
